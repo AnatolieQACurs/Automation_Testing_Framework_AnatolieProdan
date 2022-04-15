6533c467118afaf0c3af325d317552738a847375
AnatolieQACurs <101135285+AnatolieQACurs@users.noreply.github.com>
2022-04-15 16:50:14 +0300

    Add the structure of the UI test automation fremework. Including directories and TestRunner class. Also, there were added .gitignore file and detalis foe redme.md file.

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,2 +1,18 @@
+import managers.WebDriverManager;
+
 public class TestRunner {
+    public static void main(String[] args) {
+        WebDriverManager webDriverManager = new WebDriverManager("CHROME");
+
+        webDriverManager.getDriver().get("https://google.md/");
+        webDriverManager.getDriver().close();
+
+        WebDriverManager firefoxDriverManager = new WebDriverManager("CHROME");
+        firefoxDriverManager.getDriver().get("https://ltconstantinstamati.netlify.app/");
+        firefoxDriverManager.getDriver().close();
+
+//        WebDriverManager firefoxDriverManager = new WebDriverManager("FIREFOX");
+//        firefoxDriverManager.getDriver().get("https://google.md/");
+//        firefoxDriverManager.getDriver().close();
+    }
 }
